d73c7ab2820fd57e8d555d640e835b1db34595c2
sankaraxi <sankarfrompalani@gmail.com>
2024-06-22 12:05:02 +0530

    written my first code in java script

---
+++ javascript_basics/firstJSfile.js
@@ -0,0 +1,9 @@
+console.log('My first java script file')
+
+console.log("I'm Sankar"); 
+
+console.log("%cI'm Sankar","color: blue; font-size: 40px");
+
+console.log("Hello " + "there, " + "World")
+
+console.log("Hello ", "there,", "World")
