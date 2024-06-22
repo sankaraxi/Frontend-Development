bc586d00eeb5a2b79e1ecd44c13f561967bb000f
sankaraxi <sankarfrompalani@gmail.com>
2024-06-22 14:43:28 +0530

    learned javascript fundamentals

---
+++ javascript_basics/operators.js
@@ -0,0 +1,15 @@
+//arthimetic operators
+console.log(2+3);
+console.log(233-33);
+console.log(40*10);
+console.log(45/9);
+console.log(2+20-4+3*40/10);
+
+//comparison  operators
+console.log(); //place holder
+
+console.log(2<3);
+console.log(233>33);
+console.log(40==10);
+console.log(45!=9);
+console.log(22 == 22);
